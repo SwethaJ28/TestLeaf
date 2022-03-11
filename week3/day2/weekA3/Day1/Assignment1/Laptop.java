175dc3517d13127c6073e054a2069c460863ea56
SwethaJ28 <100467927+SwethaJ28@users.noreply.github.com>
2022-03-11 18:26:41 +0530

    Delete week3/day2/weekA3 directory

--- week3/day2/weekA3/Day1/Assignment1/Laptop.java
+++
@@ -1,9 +0,0 @@
-package week3.Day1.Assignment1;
-
-public class Laptop {
-	public void computerModel() {
-		// TODO Auto-generated method stub
-		System.out.println("The Computer Model is DESKTOP-T8IPD1H - Laptop");
-	}
-
-}
